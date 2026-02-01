1618cbebda613c037fcb9edeaa6184ba9d00f0bd
joowon-jang <wndnjs5248@hanmail.net>
2026-02-01 18:29:11 +0900

    feat: tanstack/react0query 패키지 추가 및 QueryClientProvider 설정

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
 import 'assets/css/index.scss';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   // <React.StrictMode> removed to fix YouTube Iframe double-mount issues
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <QueryClientProvider client={queryClient}>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </QueryClientProvider>,
   // </React.StrictMode>
 );
